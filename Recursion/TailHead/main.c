0adbd0a60ce24c5f36c35098b56115c824029582
knasrallah <knasrallah94@gmail.com>
2020-04-24 21:54:43 +0200

    adding Head and Tail recursive functions

--- Recursion/TailHead/main.c
+++ Recursion/TailHead/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int funHead (int n)
 {
-    printf("Hello world!\n");
+    if (n>0)
+    {
+        printf("%d ",n);
+        funHead(n-1);
+    }
+
+    return 0;
+}
+int funTail (int n)
+{
+    if (n>0)
+    {
+
+        funTail(n-1);
+        printf("%d ",n);
+    }
+
+    return 0;
+}
+int main()
+{   int x = 3;
+
+    funHead(x);
+    printf("\n");
+    funTail(x);
     return 0;
 }
